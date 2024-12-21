54feb7048e6befd160816159ab9e15aef8acb2ae
David Agredano <davidagredano@gmail.com>
2024-12-21 15:06:52 +0100

    Update ft_strlcat

--- tests/ft_strlcat.c
+++ tests/ft_strlcat.c
@@ -6,71 +6,41 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 12:26:30 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/20 15:47:23 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/21 15:06:04 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_tests.h"
 
-static int	ft_assert_dest(char *dst_std, char *dst_mine, char *src, size_t size)
+static int	ft_assert_dest(char *test_description, char *src, size_t size)
 {
-	int	pass;
+	char	dst_std[32] = "hello ";
+	char	dst_mine[32] = "hello ";
+	bool	pass = 1;
 
-	pass = 1;
-	dst_std = malloc(100);
-	dst_mine = malloc(100);
-	if (!dst_std || !dst_mine)
-	{
-		printf("Error: Malloc returned NULL.\n");
-		return (0);
-	}
 	strlcat(dst_std, src, size);
 	ft_strlcat(dst_mine, src, size);
-	if (strcmp(dst_std, dst_mine) != 0)
-	{
-		printf("Error:\t");
-		printf("Exected '%s' got '%s'\n", dst_std, dst_mine);
-		pass = 0;
-	}
-	else
-	{
-		printf("Pass:\t");
-		printf("Exected '%s' got '%s'\n", dst_std, dst_mine);
-	}
-	free(dst_std);
-	free(dst_mine);
+
+	if (strncmp(dst_std, dst_mine, size) != 0) pass = 0;
+	printf("- %s:\n", test_description);
+	ft_log_result_str(dst_std, dst_mine, pass);
+
 	return (pass);
 }
 
-static size_t	ft_assert_return(char *dst_std, char *dst_mine, char *src, size_t size)
+static size_t	ft_assert_return(char *test_description, char *src, size_t size)
 {
-	size_t	ret_val_std;
-	size_t	ret_val_mine;
-	int	pass;
+	char	dst_std[32] = "hello ";
+	char	dst_mine[32] = "hello ";
+	bool	pass = 1;
 
-	pass = 1;
-	dst_std = malloc(100);
-	dst_mine = malloc(100);
-	if (!dst_std || !dst_mine)
-	{
-		printf("Error: Malloc returned NULL.\n");
-		return (0);
-	}
-	ret_val_std = strlcat(dst_std, src, size);
-	ret_val_mine = ft_strlcat(dst_mine, src, size);
-	if (ret_val_std != ret_val_mine)
-	{
-		printf("Error:\t");
-		printf("Exected '%zu' got '%zu'\n", ret_val_std, ret_val_mine);
-		pass = 0;
-	}
-	else
-	{
-		printf("Pass:\t");
-		printf("Exected '%zu' got '%zu'\n", ret_val_std, ret_val_mine);
-	}
-	free(dst_std);
-	free(dst_mine);
+	size_t ret_std = strlcat(dst_std, src, size);
+	size_t ret_mine = ft_strlcat(dst_mine, src, size);
+
+	if (ret_std != ret_mine) pass = 0;
+	printf("- %s:\n", test_description);
+	ft_log_result_nbr(ret_std, ret_mine, pass);
+
 	return (pass);
 }
 
@@ -84,20 +54,15 @@ void	ft_strlcat_test(void)
 	dst_std = NULL;
 	dst_mine = NULL;
 	
-	if (!ft_assert_dest(dst_std, dst_mine, "hello", 6))
-		pass = 0;
-	if (!ft_assert_dest(dst_std, dst_mine, "hello", 2))
-		pass = 0;
-	if (!ft_assert_dest(dst_std, dst_mine, "hello", 12))
-		pass = 0;
-	if (!ft_assert_return(dst_std, dst_mine, "hello", 6))
-		pass = 0;
-	if (!ft_assert_return(dst_std, dst_mine, "hello", 2))
-		pass = 0;
-	if (!ft_assert_return(dst_std, dst_mine, "hello", 12))
-		pass = 0;
-	if (!ft_assert_return(dst_std, dst_mine, "hello", 0))
+	if (!ft_assert_dest("Dest with simple test", "world", 32) ||
+		!ft_assert_return("Return value with simple test", "world", 32) ||
+		!ft_assert_dest("Dest with size(9) less than src + dest(12)", "world", 9) ||
+		!ft_assert_return("Return value with size(9) less than src + dest(12)", "world", 9) ||
+		!ft_assert_dest("Dest with size(3) less than src(6)", "world", 3) ||
+		!ft_assert_return("Return value with size(3) less than src(6)", "world", 3) ||
+		!ft_assert_dest("Dest with size 0", "world", 0) ||
+		!ft_assert_return("Return value with size 0", "world", 0))
 		pass = 0;
 
-	ft_print_test_result("ft_strlcpy", pass);
+	ft_log_result_test("ft_strlcat", pass);
 }
