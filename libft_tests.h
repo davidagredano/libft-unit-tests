691b7562cbf91adc914eae4468625ceb12c5a26a
David Agredano <davidagredano@gmail.com>
2024-12-28 09:40:43 +0100

    Add ft_lstsize

--- libft_tests.h
+++ libft_tests.h
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:40:36 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/27 21:10:52 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/28 09:33:35 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list  *ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 void	ft_isalpha_test(void);
 void	ft_isdigit_test(void);
--- tests/bonus.c
+++ tests/bonus.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 19:38:25 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/27 20:56:58 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/28 09:38:46 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,22 @@ static bool	ft_test(void *s1, void *s2)
 {
 	bool	pass = 1;
 
-	//ft_lstnew
+	//ft_lstnew (1)
 	t_list *begin = ft_lstnew(strdup(s1));
 	if (memcmp(s1, begin->content, strlen(s1) + 1)) pass = 0;
 	if (begin->next != NULL) pass = 0;
 
-	//ft_lstadd_front
+	//ft_lstadd_front (2)
 	t_list *new = ft_lstnew(strdup(s2));
 	ft_lstadd_front(&begin, new);
 	if (memcmp(s2, begin->content, strlen(s2) + 1)) pass = 0;
 	if (begin->next == NULL) pass = 0;
 
+	//log list
 	ft_log_list_contents(begin);
+
+	//ft_lstsize (3)
+	printf("List size: %d\n", ft_lstsize(begin));
 	
 	//free the list 
 	t_list	*next = begin;
