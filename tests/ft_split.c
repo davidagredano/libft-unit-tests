20b1c0d94f103474799109a6956d3cf6a711a080
David Agredano <davidagredano@gmail.com>
2024-12-29 12:27:19 +0100

    Update ft_split tests

--- tests/ft_split.c
+++ tests/ft_split.c
@@ -6,41 +6,41 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 15:18:01 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/24 19:08:16 by dagredan         ###   ########.fr       */
+/*   Updated: 2024/12/29 12:15:09 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_tests.h"
 
+static void	ft_log_split_result(const char *s, char c, char **substrs)
+{
+	size_t	i = 0;
 
-static bool	ft_log_test(char const *s, char c)
+	printf("\nResult for \"%s\" '%c':\n", s, c);
+	while (substrs[i] != NULL)
+	{
+		printf("%zu. %s\n", i, substrs[i]);
+		i++;
+	}
+	printf("%zu. %s\n", i, substrs[i]);
+}
+
+static bool	ft_test(char const *s, char c, const char *expect)
 {
 	bool	pass = 1;
-	size_t	i;
+	size_t	i = 0;
 
-	char **ret_mine = ft_split(s, c);
+	char **substrs = ft_split(s, c);
 
-	//if (strcmp(expect, ret_mine) != 0) pass = 0;
-	//ft_log_result_str(expect, ret_mine, s, pass);
+	if (strcmp(expect, substrs[0]) != 0) pass = 0;
+	//ft_log_split_result(s, c, substrs);
 
-	/*
-	printf("\nResult for '%s':\n", s);
-	i = 0;
-	while (ret_mine[i] != NULL)
+	while (substrs[i] != NULL)
 	{
-		printf("%zu. %s\n", i, ret_mine[i]);
+		free(substrs[i]);
 		i++;
 	}
-	printf("%zu. (NULL)\n", i);
-	*/
-
-	i = 0;
-	while (ret_mine[i] != NULL)
-	{
-		free(ret_mine[i]);
-		i++;
-	}
-	free(ret_mine);
+	free(substrs);
 	return (pass);
 }
 
@@ -48,10 +48,11 @@ void	ft_split_test(void)
 {
 	int		pass = 1;
 	
-	if (!ft_log_test("Hello world", ' ') ||
-		!ft_log_test("Hi-bye-hello", '-') ||
-		!ft_log_test("Hello", ' ') ||
-		!ft_log_test("This is a super string", ' '))
+	if (!ft_test("Hello world", ' ', "Hello") ||
+		!ft_test("Hi-bye-hello", '-', "Hi") ||
+		!ft_test("Hello", ' ', "Hello") ||
+		!ft_test("**hello***students*how**are****you?*", '*', "hello") ||
+		!ft_test("This is a super string", ' ', "This"))
 		pass = 0;
 
 	ft_log_result_test("ft_split", pass);
