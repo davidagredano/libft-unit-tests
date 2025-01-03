37fc1f03e8d1ee8969cacde36feb6f8fd5217fd7
David Agredano <davidagredano@gmail.com>
2025-01-03 10:59:54 +0100

    Test ft_putstr_fd with NULL input

--- tests/ft_putstr_fd.c
+++ tests/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 11:57:34 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/27 12:29:29 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/03 10:55:28 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ void	ft_putstr_fd_test(void)
 	if (!ft_test("'hello'", "hello", STDOUT_FILENO) ||
 		!ft_test("'HELLO'", "HELLO", STDOUT_FILENO) ||
 		!ft_test("'heLLo'", "heLLo", STDOUT_FILENO) ||
+		!ft_test("''", "", STDOUT_FILENO) ||
+		!ft_test("NULL", NULL, STDOUT_FILENO) ||
 		!ft_test("'000'", "000", STDOUT_FILENO))
 		pass = 0;
 
