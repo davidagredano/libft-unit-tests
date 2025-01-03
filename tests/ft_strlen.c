fa90cdf357de1422811ccf5d705d07f13bd36947
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-03 15:32:51 +0100

    Update ft_strlen tests

--- tests/ft_strlen.c
+++ tests/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 12:35:47 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/21 11:12:39 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/03 15:32:21 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 void	ft_strlen_test(void)
 {
-	int		len = 5;
-	char	*strs[] = {"hello", "world", "a", "", "lkjh  23ew"};
-	int		i = 0;
 	int		pass = 1;
+	char	*strs[] = {"hello", "a", "", "lala lala"/*, NULL*/};
+	int		len = 4;
+	int		i = 0;
+
+	size_t	ret_std;
+	size_t	ret_mine;
 
 	while (i < len)
 	{
-		if (ft_strlen(strs[i]) != strlen(strs[i]))
-		{
+		ret_std = strlen(strs[i]);
+		ret_mine = ft_strlen(strs[i]);
+		if (ret_mine != ret_std)
 			pass = 0;
-			printf("Error for input %s. ", strs[i]);
-			printf("Expected %zu ", strlen(strs[i]));
-			printf("instead of %zu\n", ft_strlen(strs[i]));
-		}
+		//ft_log_result_nbr(ret_std, ret_mine, strs[i], pass);
 		i++;
 	}
+
 	ft_log_result_test("ft_strlen", pass);
 }
