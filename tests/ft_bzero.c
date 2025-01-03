7cfb419b7893225021296a221e314272be4bf993
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-03 15:26:54 +0100

    Update ft_bzero

--- tests/ft_bzero.c
+++ tests/ft_bzero.c
@@ -6,17 +6,15 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 14:39:05 by dagredan          #+#    #+#             */
-/*   Updated: 2024/12/21 11:09:50 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/02 16:10:14 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_tests.h"
 
-static int	ft_assert(void *s1, void *s2, size_t n)
+static int	ft_assert(void *s1, void *s2, size_t n, char *desc)
 {
-	int	pass;
-
-	pass = 1;
+	int	pass = 1;
 	s1 = malloc(n);
 	s2 = malloc(n);
 	if (!s1 || !s2)
@@ -24,34 +22,41 @@ static int	ft_assert(void *s1, void *s2, size_t n)
 		printf("Error: Malloc returned NULL.\n");
 		return (0);
 	}
+
 	bzero(s1, n);
 	ft_bzero(s2, n);
+
 	if (memcmp(s1, s2, n) != 0)
-	{
 		pass = 0;
-		printf("Error: ");
-		printf("Expected '%s' ", (char *) s1);
-		printf("instead of '%s'\n", (char *) s2);
-	}
+
+	//ft_log_result_str((char *) s1, (char *) s2, desc, pass);
+
 	free(s1);
 	free(s2);
 	return (pass);
 }
 
+static void	ft_test_null(void)
+{
+	bzero(NULL, 5);
+	ft_bzero(NULL, 5);
+}
+
 void    ft_bzero_test(void)
 {
-	int	pass;
+	int		pass = 1;
 	void	*s1;
 	void	*s2;
 
-	pass = 1;
 	s1 = NULL;
 	s2 = NULL;
 
-	if (!ft_assert(s1, s2, 987325))
+	if (!ft_assert(s1, s2, 987325, "987325") ||
+		!ft_assert(s1, s2, 42, "42") ||
+		!ft_assert(s1, s2, 0, "0"))
 		pass = 0;
-	if (!ft_assert(s1, s2, 42))
-		pass = 0;
+
+	//ft_test_null(); // Should segfault
 
 	ft_log_result_test("ft_bzero", pass);
 }
